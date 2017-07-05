94d6300744bf0317c8a18e6ec1ed4f17a9bb66c6
Zrezi <zbrezi@gmail.com>
2017-07-05 11:21:42 -0500

    0705172 - Programming Log update

--- js/footer-text.js
+++ js/footer-text.js
@@ -1,3 +1,11 @@
 (function() {
-	document.getElementById("footer-text").innerHTML = "0705171";
+	var a = document.createElement("a");
+	a.innerHTML = "0705172";
+	a.href = "";
+	a.onclick = function(event) {
+		event.preventDefault();
+		window.location = "/pages/programminglog.html";
+	}
+	
+	document.getElementById("footer-text").appendChild(a);
 })();
